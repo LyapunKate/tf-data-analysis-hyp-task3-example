aebd5eaf4f46bf774dafba885c655b1232d86ad3
LyapunKate <91290812+LyapunKate@users.noreply.github.com>
2023-04-14 00:50:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,12 +7,13 @@ chat_id = 532569024
 
 def solution(sample):
     import math
+    from scipy.stats import t
     threshold = 500
     significance_level = 0.06
     n = len(sample)
     x_bar = sum(sample) / n
     s = math.sqrt(sum((x - x_bar) ** 2 for x in sample) / (n - 1))
     t_statistic = (x_bar - threshold) / (s / math.sqrt(n))
-    t_critical = abs(stats.t.ppf(significance_level / 2, n - 1))
-    p_value = 2 * (1 - stats.t.cdf(abs(t_statistic), n - 1))
+    t_critical = abs(t.ppf(significance_level / 2, n - 1))
+    p_value = 2 * (1 - t.cdf(abs(t_statistic), n - 1))
     return t_statistic > t_critical
