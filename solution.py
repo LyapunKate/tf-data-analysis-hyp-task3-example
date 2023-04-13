96e413ac0aff58d851e05986924d1deb917842c4
LyapunKate <91290812+LyapunKate@users.noreply.github.com>
2023-04-13 23:58:39 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,15 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 532569024
+ # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(sample, threshold, significance_level):
+    import math
+    n = len(sample)
+    x_bar = sum(sample) / n
+    s = math.sqrt(sum((x - x_bar) ** 2 for x in sample) / (n - 1))
+    t_statistic = (x_bar - threshold) / (s / math.sqrt(n))
+    t_critical = abs(stats.t.ppf(significance_level / 2, n - 1))
+    p_value = 2 * (1 - stats.t.cdf(abs(t_statistic), n - 1))
+    return t_statistic > t_critical, p_value
